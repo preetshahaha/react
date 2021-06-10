f65db2816d584a09ec50add02ada5a76c44bbde1
Preet shah <69816411+preetshahaha@users.noreply.github.com>
2021-06-11 02:20:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <input type="text" id="inputText"/>
+     <br></br>
+     <button>save</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyBKnyE_6crQRN7rk_0s4U2ufSIKHLZm1zc",
+    authDomain: "todo-73527.firebaseapp.com",
+    projectId: "todo-73527",
+    storageBucket: "todo-73527.appspot.com",
+    messagingSenderId: "165364346459",
+    appId: "1:165364346459:web:391b0a403623677b20197e",
+    measurementId: "G-5J9D3012HK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+export default firebase;
